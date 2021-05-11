9bac63d689f0c01987698ea0df803e0938081324
Gayeong Do <dodoga096@gmail.com>
2021-05-11 23:21:43 +0900

    Update

--- Algorithm/src/Level1/NumberOfK.java
+++ Algorithm/src/Level1/NumberOfK.java
@@ -7,9 +7,7 @@ public class NumberOfK {
         int[] answer = {};
 
         for (int i = 0; i < commands.length; i++) {
-            int start = commands[i][0];
-            int end = commands[i][1];
-            int[] temp = {};
+            int[] temp = new int[commands[i][1]-commands[i][0]+1];
             for (int j = 0; j < end-start+1; j++) {
                 temp[j] = array[j];
                 Arrays.sort(temp);
