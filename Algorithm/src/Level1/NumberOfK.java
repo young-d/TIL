25324a7b40ecb71ba567f22d427683229f4227f5
Gayeong Do <dodoga096@gmail.com>
2021-05-12 23:10:57 +0900

    Update

--- Algorithm/src/Level1/NumberOfK.java
+++ Algorithm/src/Level1/NumberOfK.java
@@ -4,15 +4,15 @@ import java.util.Arrays;
 
 public class NumberOfK {
     public static int[] solution(int[] array, int[][] commands) {
-        int[] answer = {};
+        int[] answer = new int[commands.length];
 
         for (int i = 0; i < commands.length; i++) {
             int[] temp = new int[commands[i][1]-commands[i][0]+1];
-            for (int j = 0; j < end-start+1; j++) {
-                temp[j] = array[j];
-                Arrays.sort(temp);
-                answer[i] = temp[commands[i][2]];
+            for (int j = 0; j < temp.length; j++) {
+                temp[j] = array[j+commands[i][0]-1];
             }
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2]-1];
         }
         return answer;
     }
