484a640b75a63cffeab0c382b1efdb97b3da8529
Gayeong Do <dodoga096@gmail.com>
2021-05-28 22:40:20 +0900

    정보처리기사

---
+++ 정보처리기사/06.프로그래밍언어활용/C/main01.c
@@ -0,0 +1,28 @@
+#include <studio.h>
+void main() {
+    int i, j;
+    int tmp;
+    int a[5] = {75, 95, 85, 100, 50}
+
+    for(i=0; i<4; i++) {
+        for(j=0; j<4-i; j++) {
+            if(a[j] > a[j + 1]) {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;            
+            }
+        }
+    }
+
+    for(i=0; i<5; i++) {
+        printf("%d", a[i]);
+    }
+}
+
+
+
+
+/*
+bubble sort : 오름차순
+a = [50, 75, 85, 95, 100]
+*/
