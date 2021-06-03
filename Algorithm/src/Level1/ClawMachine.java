cddd8b21b2121c49c2764301b62a59f77deb1fa0
Gayeong Do <70619304+young-d@users.noreply.github.com>
2021-06-03 23:47:43 +0900

    fix: solve conflict

--- Algorithm/src/Level1/ClawMachine.java
+++ Algorithm/src/Level1/ClawMachine.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package level1;
 
 import java.util.Stack;
@@ -43,47 +42,3 @@ public class ClawMachine {
 
 
 }
-=======
-package level1;
-
-import java.util.Stack;
-
-public class ClawMachine {
-    public static int solution(int[][] board, int[] moves) {
-
-        Stack<Integer> stack = new Stack<>();
-        int result = 0;
-        int line = 0;
-
-        for(int i = 0; i < moves.length; i++) {
-            line = moves[i];
-            for(int j = 0; j < board.length; j++) {
-                if(board[j][line-1] != 0) {
-                    if (!stack.isEmpty() && board[j][line - 1] == stack.peek()) {
-                        stack.pop();
-                        result += 2;
-                    } else {
-                        stack.push(board[j][line - 1]);
-                    }
-                    board[j][line-1] = 0;
-                    break;
-                }
-            }
-        }
-        return result;
-    }
-
-    public static void main(String[] args) {
-        int[][] board = {
-                {0,0,0,0,0},
-                {0,0,1,0,3},
-                {0,2,5,0,1},
-                {4,2,4,4,2},
-                {3,5,1,3,1}
-        };
-        int[] moves = {1,5,3,5,1,2,1,4};
-
-        solution(board, moves);
-    }
-}
->>>>>>> c07b6628236faf281aa17ce38652dbae08f264ef
