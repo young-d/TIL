077f3b08832f4224ee415f317cf848a60fc1d02e
Gayeong Do <dodoga096@gmail.com>
2021-06-04 00:06:13 +0900

    Coding test: ClawMachine by JS - Stack

--- coding_test/javascript/ClawDolls.js
+++ coding_test/javascript/ClawDolls.js
@@ -4,14 +4,14 @@ function solution(board, moves) {
 
     for(let i=0; i < moves.length; i++) {
         let col = moves[i]-1;
-        for(let row = 0; row < 5; row++) {
+        for(let row = 0; row < board.length; row++) {
             let doll = board[row][col];
             if(board[row][col] !== 0) {
                 if(bucket.length !== 0 && doll === bucket[bucket.length-1]) {
                     bucket.pop();
                     answer+=2;
                 }else {
-                    bucket.push(board[row][col]);
+                    bucket.push(doll);
                 }
                 board[row][col] = 0;
                 break;
