190e5383dcb4a710416812912db2203051a06a7f
Gayeong Do <dodoga096@gmail.com>
2021-06-14 15:49:34 +0900

    Fix : JS 문자열 다루기 방법 추가

--- coding_test/programmers/StringHandling.js
+++ coding_test/programmers/StringHandling.js
@@ -3,19 +3,35 @@ function solution(s) {
 
     if(s.length === 4 || s.length === 6) {
         for(let i = 0; i < s.length; i++) {
-            if(Number(s[i]) != s[i]) {
-                answer = false;
-                break;
-            }
+            //1.
             // for(let j = 0; j <= 9; j++) {
             //     if(s[i] == j) {
             //         answer = true;
             //         break;
             //     }
             //     answer = false;
-                
             // }
             // if(answer === false) return answer;
+
+            //2.
+            // if(Number(s[i]) != s[i]) {
+            //     answer = false;
+            //     break;
+            // }
+
+            //3.
+            // if(!(s.charCodeAt(i) >= '0'.charCodeAt(0) &&  s.charCodeAt(i)<= '9'.charCodeAt(0))) {
+            //     answer = false;
+            //     break;
+            // }
+
+            //4.
+            if(!Number(s[i])) {
+                answer = false;
+                break;
+            }
+
+            
         }
     }else {
         answer = false;
@@ -25,5 +41,6 @@ function solution(s) {
 }
 
 
-console.log(solution("a234"));
-console.log(solution("1234"));
+console.log(solution("a234"));  //false
+console.log(solution("1234"));  //true
+
