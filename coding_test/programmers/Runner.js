d6bdb1ac857ccb923ff39a8e477b0f6bc279e456
Gayeong Do <dodoga096@gmail.com>
2021-06-18 18:16:22 +0900

    Coding test : 프로그래머스 level1 by JS - 완주하지 못한 선수

---
+++ coding_test/programmers/Runner.js
@@ -0,0 +1,30 @@
+function solution(participant, completion) {
+    let answer = '';
+    let obj = {};
+    
+
+    
+    for(let p of participant) {
+        if(!obj[p]) {
+            obj[p] = 1;
+        }else {
+            obj[p] += 1;
+        }
+    }
+    
+    for(let c of completion) {
+        obj[c] -= 1;
+    }    
+    
+    for(let p of participant) {
+        if(obj[p] !== 0) {
+            answer = p;
+        }
+    }
+    
+    
+    
+    return answer;
+}
+
+console.log(solution(["leo", "kiki", "eden"],["eden", "kiki"]));
