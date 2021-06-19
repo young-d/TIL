3f04fd0b0e5d38691c42a4addf2b9bcc81a6f6d1
Gayeong Do <dodoga096@gmail.com>
2021-06-19 22:31:29 +0900

    Coding test: Data structure by JS - 피보나치수열 - 메모이제이션

---
+++ coding_test/javascript/Fibonacci.js
@@ -0,0 +1,13 @@
+let numbers = [];
+function fibo(n) {
+    if(numbers[n] > 0) {
+        return numbers[n];  //이미 구한 수는 재귀호출 없이 저장된 값 리턴
+    }else if(n === 1|| n === 2) {
+        return numbers[n] = 1;
+    }else {
+        return numbers[n] = fibo(n - 2) + fibo(n - 1); 
+    }
+}
+
+let n = 45;
+for(let i = 1; i <= n; i++) console.log(`${fibo(i)}\n`);
