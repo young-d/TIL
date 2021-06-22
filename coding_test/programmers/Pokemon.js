b7a4afeb742018c9f57b57ccebfa0eef4e6e7457
Gayeong Do <dodoga096@gmail.com>
2021-06-22 20:15:58 +0900

    Coding test: 프로그래머스 level1 by JS - 폰켓몬 - 중복제거 -> Set

--- coding_test/programmers/Pokemon.js
+++ coding_test/programmers/Pokemon.js
@@ -1,15 +1,20 @@
 function solution(nums) {
-    let answer = 0;
+    //1. object 사용해서 중복되지 않는 요소 개수 구하기
+    // let answer = 0;
+    // let obj = nums.reduce((kinds, element) => {
+    //     if(!kinds[element]) {
+    //         kinds[element] = 1;
+    //         answer++;
+    //     }
+    //     return kinds;
+    // }, {});
 
-    let obj = nums.reduce((kinds, element) => {
-        if(!kinds[element]) {
-            kinds[element] = 1;
-            answer++;
-        }
-        return kinds;
-    }, {});
+    // return answer > nums.length / 2 ? nums.length / 2 : answer;
 
-    return answer > nums.length / 2 ? nums.length / 2 : answer;
+    //2. set으로 중복 제거한 후 요소 개수 반환
+    let tmp = new Set(nums);
+
+    return [...tmp].length > nums.length / 2 ? nums.length / 2 : [...tmp].length;
 }
 
 console.log(solution([3,1,2,3]));
