8c750f4bb5854196827f49fa9cee46356838e063
Gayeong Do <dodoga096@gmail.com>
2021-06-23 00:53:15 +0900

    Coding test: 프로그래머스 level1 by JS - 같은 숫자는 싫어

--- coding_test/programmers/RemoveDuplication.js
+++ coding_test/programmers/RemoveDuplication.js
@@ -1,11 +1,9 @@
-function solution(arr)
-{
+function solution(arr) {
     var answer = [];
     let n = 0;
 
     for(let i = 0; i < arr.length; i++) {
         if(answer[answer.length - 1] !== arr[i]) answer.push(arr[i]);
-        else continue;
     }
 
     return answer;
