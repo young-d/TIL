92cff384d024944332d759c3c6bb9174a445f158
DoGayeong <dodoga096@gmail.com>
2021-06-30 22:45:38 +0900

    Revert "# <타입>: <제목>"
    This reverts commit bab72584143921aeb8fce06481c01f44755161e4.

--- coding_test/programmers/DivideNumber.js
+++
@@ -1,9 +0,0 @@
-function solution(arr, divisor) {
-    let answer = [];
-
-    for(let number of arr) {
-        if(number % divisor === 0) answer.push(number);
-    }
-
-    return answer.length === 0 ? [-1] : answer.sort((a, b) => a - b);
-}
