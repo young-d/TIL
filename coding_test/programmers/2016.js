a5658a52a4ec8b63dc429e1ef79cfc46d48eeb56
DoGayeong <dodoga096@gmail.com>
2021-06-30 22:44:12 +0900

    Coding test: 프로그래머스 level1 by JS
    - 2016년

--- coding_test/programmers/2016.js
+++ coding_test/programmers/2016.js
@@ -1,4 +1,28 @@
 function solution(a, b) {
-    var answer = '';
+    let answer = '';
+    let day = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    let aux = 0;
+    for(let i = 0; i < a; i++) {
+        aux += day[i];
+    }
+
+    switch((aux + b) % 7) {
+        case 1: answer = 'FRI';
+                break;
+        case 2: answer = 'SAT';
+                break;
+        case 3: answer = 'SUN';
+                break;
+        case 4: answer = 'MON';
+                break;
+        case 5: answer = 'TUE';
+                break;
+        case 6: answer = 'WED';
+                break;
+        case 0: answer = 'THU';
+                break;
+    }
+
     return answer;
 }
