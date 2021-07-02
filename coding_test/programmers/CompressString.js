a4f5150d9c9dfab8ea4090d19982aa95256b96d8
DoGayeong <dodoga096@gmail.com>
2021-07-02 17:10:41 +0900

    Coding test: 프로그래머스 level2 by JS
    - 문자열 압축
    - 테스트케이스는 통과 / 채점 시 실패

--- coding_test/programmers/CompressString.js
+++ coding_test/programmers/CompressString.js
@@ -1,32 +1,72 @@
 function solution(s) {
     let answer = 0;
-    let compression = '';
+    let result = [];
 
-    for(let i = s.split('').length; i >= 1; i--) {
-        if(s.split('').length / i < 2) continue;
-        
-        let pattern = s.slice(0, i);
+    compress(s);
+
+    answer = result[0];
+
+    function compress(arr, compression = '', unit = 0) {
+        if(arr.length < 2) return;
+
+        let len = arr.length;
+        let pattern = '';
         let cnt = 0;
+        let rest = '';
 
-        console.log(pattern, i);
-
-        let start = i;
-        for(let j = 1; j < Math.floor(s.split('').length / i); j++) {
-            if(s.slice(start * j, start * j + i) === pattern) {
-                cnt++;
+        if(unit === 0) {
+            
+            for(let i = len; i >= 1; i--) {
+                if(len / i < 2) continue;
+                
+                pattern = arr.slice(0, i);
+                
+                for(let part = 1; part < Math.floor(len / i); part++) {
+                    if(arr.slice(i * part, i * (part + 1)) === pattern) {
+                        cnt++;
+                    }else {
+                        break;
+                    }
+                }   
+                
+                if(cnt > 0) {
+                    compression += (cnt + 1) + pattern;
+                    break;
+                }
             }
-        }   
+            
+            unit = pattern.length;
+        }else {
+            pattern = arr.slice(0, unit);
+    
+            for(let part = 1; part < Math.floor(len / unit); part++) {
+               if(arr.slice(unit * part, unit * (part + 1)) === pattern) {
+                    cnt++;
+                }else {
+                    break;
+                }
+            }   
+            
+            if(cnt > 0) {
+                compression += (cnt + 1) + pattern;
+            }
 
-        if(cnt > 0) {
-            compression = (cnt + 1) + pattern + s.slice( (cnt + 1) * i, );
-            console.log(compression, cnt);
-            answer = compression.split('').length;
-            break;
         }
+        
+        if(cnt === 0) compression += pattern;
+        rest = arr.slice((cnt + 1) * unit, );
+        
+        if(rest.length >= 2 * unit) {
+            result.push(compress(rest, compression, unit).length);
+        }else {
+            result.push((compression + rest).length);
+        }
+        console.log(pattern, compression, rest, unit, compression + rest, result);
+        
+        return compression + rest;
     }
 
-
-    return answer > 0 ? answer : s.split('').length;
+    return answer;
 }
 
-solution('ababcdcdababcdcd');
+console.log(solution('ababcdcdababcdcd'));
