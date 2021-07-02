9ccbb85790b22171780139e68611ffc88d9ef7ba
DoGayeong <dodoga096@gmail.com>
2021-07-02 23:53:19 +0900

    Coding test: 프로그래머스 level2 by JS
    - 문자열 압축

--- coding_test/programmers/CompressString.js
+++ coding_test/programmers/CompressString.js
@@ -2,68 +2,32 @@ function solution(s) {
     let answer = 0;
     let result = [];
 
-    compress(s);
+    //압축 단위
+    for(let i = 1; i < (s.length / 2 + 1); i++) {
+        let cnt = 1;
+        let aux = '';
+        //문자열 끝까지 단위별로 돌기
+        for(let j = 0; j < s.length; j++) {
+            let now = s.substr(j * i, i);
+            let next = s.substr((j * i) + 1, i);
+
+            if(now === next) {
+                cnt++;
+            }else {
+                if(cnt > 1) {
+                    aux += cnt + now;
+                }else {
+                    aux += now;
+                }
+                cnt = 1;
+            }
+        }
+        result.push(aux.length);
+    }
 
     answer = result[0];
-
-    function compress(arr, compression = '', unit = 0) {
-        if(arr.length < 2) return;
-
-        let len = arr.length;
-        let pattern = '';
-        let cnt = 0;
-        let rest = '';
-
-        if(unit === 0) {
-            
-            for(let i = len; i >= 1; i--) {
-                if(len / i < 2) continue;
-                
-                pattern = arr.slice(0, i);
-                
-                for(let part = 1; part < Math.floor(len / i); part++) {
-                    if(arr.slice(i * part, i * (part + 1)) === pattern) {
-                        cnt++;
-                    }else {
-                        break;
-                    }
-                }   
-                
-                if(cnt > 0) {
-                    compression += (cnt + 1) + pattern;
-                    break;
-                }
-            }
-            
-            unit = pattern.length;
-        }else {
-            pattern = arr.slice(0, unit);
-    
-            for(let part = 1; part < Math.floor(len / unit); part++) {
-               if(arr.slice(unit * part, unit * (part + 1)) === pattern) {
-                    cnt++;
-                }else {
-                    break;
-                }
-            }   
-            
-            if(cnt > 0) {
-                compression += (cnt + 1) + pattern;
-            }
-
-        }
-        
-        if(cnt === 0) compression += pattern;
-        rest = arr.slice((cnt + 1) * unit, );
-        
-        if(rest.length >= 2 * unit) {
-            result.push(compress(rest, compression, unit).length);
-        }else {
-            result.push((compression + rest).length);
-        }
-        console.log(pattern, compression, rest, unit, compression + rest, result);
-        
-        return compression + rest;
+    for(let r of result) {
+        if(answer > r) answer = r;
     }
 
     return answer;
