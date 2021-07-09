9ef1ff775a4fa1e05ae66dc51c66cb5af6d1c486
DoGayeong <dodoga096@gmail.com>
2021-07-09 16:33:53 +0900

    Coding test: 프로그래머스 level2 by JS
    - 구명보트
    - 탐욕법
    - 정확성/효율성 통과o

--- coding_test/programmers/LifeBoat.js
+++ coding_test/programmers/LifeBoat.js
@@ -1,39 +1,22 @@
 function solution(people, limit) {
     let answer = 0;
-    let save = Array.from({length : people.length}, () => 0);
-
     people.sort((a, b) => b - a);
+    
+    let first = 0; //가장 몸무게가 많은 사람
+    let last = people.length - 1; //가장 몸무게가 적은 사람
+    while(first <= last) {
+        //한 명이 남았을 때 보트 1개 추가해서 리턴
+        if(first === last) return answer + 1;
 
-    while(save.indexOf(0) > -1) {
-        let first = save.indexOf(0);
-        let last = save.lastIndexOf(0);
-        let total = people[first];
-
-        save[first] = 1;
-
-        //효율성 위해 추가했으나 실패
-        // if(people[first] <= Math.floor(limit / 2)) {
-        //     let rest = people.length - save.reduce((acc, element) => acc + element) + 1;
-        //     answer += Math.ceil(rest / 2);
-        //     break;
-        // }
-
-        if(total + people[last] > limit) {
-            answer++;
-            continue;
-        }
-
-        for(let i = first + 1; i <= last; i++) {
-            if(save[i] === 1 || total + people[i] > limit) continue;
-            
-            total += people[i];
-            save[i] = 1;
-            break;
-        }
-
+        //두명을 태웠을 때 제한무게보다 많으면 가장 많은 사람만 태우고 아니면 두명을 태우
+        if(people[first] + people[last] > limit) last++;
+           
+        first++; 
         answer++;
     }
 
     return answer;
 }
 
+
+console.log(solution([70, 80, 50], 100));
