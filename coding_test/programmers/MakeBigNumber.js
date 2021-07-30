0eea77229caa63c02521ddb9c095e9ba8360ddaf
DoGayeong <dodoga096@gmail.com>
2021-07-30 20:23:12 +0900

    Coding test: 프로그래머스 level2 by JS
    - 큰 수 만들기
    - 테스트중

---
+++ coding_test/programmers/MakeBigNumber.js
@@ -0,0 +1,12 @@
+function solution(number, k) {
+    let answer = '';
+    
+    answer = 'a';
+
+    // answer = number.split('').sort((a, b) => b - a).join('').slice(0, number.length - k);
+    
+    
+    return answer;
+}
+
+console.log(solution("1924", 2));
